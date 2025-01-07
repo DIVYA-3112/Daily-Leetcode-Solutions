4e1b8f8728db28e9ed2faa15f4dfd25188e25046
DIVYA-3112 <113194039+DIVYA-3112@users.noreply.github.com>
2025-01-07 11:47:39 +0530

    Time: 4 ms (31.92%) | Memory: 12.2 MB (17.90%) - LeetSync

---
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool static fun(const string& a, const string& b)
+    {
+        return (a.size() < b.size());
+    }
+    vector<string> stringMatching(vector<string>& words) {
+        int n = words.size();
+        sort(words.begin(), words.end(), fun);
+        for(auto& it : words) cout << it << " ";
+        cout << endl;
+        vector<string> ans;
+
+        for(int i=0; i<n; i++)
+        {
+            string s1 = words[i];
+            for(int j=i+1; j<n; j++)
+            {
+                string s2 = words[j];
+                if(s2.find(s1) != string::npos)
+                {
+                    ans.push_back(s1);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
