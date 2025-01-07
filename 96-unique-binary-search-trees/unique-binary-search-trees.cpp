b753b8c2225c48a7f46c2a7eb9a292c9387dd45c
DIVYA-3112 <113194039+DIVYA-3112@users.noreply.github.com>
2025-01-07 23:49:41 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (36.38%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,17 +1,5 @@
 class Solution {
 public:
-    int fun(int l, int r, vector<int> &dp)
-    {
-        if(l >= r) return 1;
-
-        int ans = 0;
-        if(dp[r-l+1] != -1) return dp[r-l+1];
-        for(int i=l; i<=r; i++)
-        {
-            ans += (fun(l,i-1,dp) * fun(i+1,r,dp));
-        }
-        return dp[r-l+1] = ans;
-    }
     int numTrees(int n) {
         vector<int> dp(n+1,0);
         int ans = 0;
