16fa814be0b14007e117cb1a48d348225e84604a
DIVYA-3112 <113194039+DIVYA-3112@users.noreply.github.com>
2025-01-07 11:50:03 +0530

    Time: 3 ms (48.52%) | Memory: 11.4 MB (60.52%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    bool static fun(const string& a, const string& b)
-    {
-        return (a.size() < b.size());
-    }
+    // bool static fun(const string& a, const string& b)
+    // {
+    //     return (a.size() < b.size());
+    // }
     vector<string> stringMatching(vector<string>& words) {
         int n = words.size();
-        sort(words.begin(), words.end(), fun);
+        // sort(words.begin(), words.end(), fun);
         vector<string> ans;
 
         for(int i=0; i<n; i++)
         {
             // string s1 = words[i];
-            for(int j=i+1; j<n; j++)
+            for(int j=0; j<n; j++)
             {
                 // string s2 = words[j];
+                if(i == j) continue;
                 if(words[j].find(words[i]) != string::npos)
                 {
                     ans.push_back(words[i]);
