b42dc8a5b7afcee6250724d007838c1d526811d8
DIVYA-3112 <113194039+DIVYA-3112@users.noreply.github.com>
2025-01-08 11:58:13 +0530

    Time: 11 ms (18.43%) | Memory: 23.7 MB (59.11%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
@@ -8,18 +8,14 @@ public:
             for(int j=i+1; j<n; j++)
             {
                 string s1,s2;
-                if(words[i].size() > words[j].size())
-                {
-                    continue;
-                }
-                else if(words[i].size() <= words[j].size())
-                {
-                    s1= words[i];
-                    s2 = words[j];
-                }
-                
+
+                s1= words[i];
+                s2 = words[j];
+
                 int n1 = s1.size();
                 int n2 = s2.size();
+
+                if(n2 < n1) continue;
 
                 string t1 = s2.substr(0,n1);
                 string t2 = s2.substr(n2-n1,n1);
