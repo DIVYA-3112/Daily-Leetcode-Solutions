40e33bd98fc70e18d3ca6b273658a76ebfd541da
DIVYA-3112 <113194039+DIVYA-3112@users.noreply.github.com>
2025-01-11 12:16:30 +0530

    Time: 25 ms (13.64%) | Memory: 15.3 MB (9.09%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.cpp
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.cpp
@@ -3,16 +3,18 @@ public:
     bool canConstruct(string s, int k) {
         int n = s.size();
         unordered_map<char,int> mp;
+        int cnt = 0;
         for(int i=0; i<n; i++)
         {
             mp[s[i]]++;
+            if(mp[s[i]] % 2 == 1) cnt++;
+            else cnt--;
         }
 
-        int cnt = 0;
-        for(auto it : mp)
-        {
-            if(it.second % 2 == 1) cnt++;
-        }
+        // for(auto it : mp)
+        // {
+        //     if(it.second % 2 == 1) cnt++;
+        // }
 
         if(cnt > k) return false;
         if(n < k) return false;
