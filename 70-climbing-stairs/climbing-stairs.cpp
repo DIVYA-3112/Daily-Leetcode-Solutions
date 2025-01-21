f68e37e3b0897c5ed7fb3838a18e79608bb9f71a
DIVYA-3112 <113194039+DIVYA-3112@users.noreply.github.com>
2025-01-21 19:29:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (32.88%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -9,7 +9,14 @@ public:
         return dp[n] = fun(n-1,dp) + fun(n-2,dp);
     }
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return fun(n,dp);
+        vector<int> dp(n+1,0);
+        // return fun(n,dp);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
