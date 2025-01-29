aad295f50910b2a545b2be9d6e07d2d9f01837cc
DIVYA-3112 <113194039+DIVYA-3112@users.noreply.github.com>
2025-01-29 18:55:40 +0530

    Time: 247 ms (5.01%) | Memory: 110 MB (20.73%) - LeetSync

---
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+
+    bool check(int r,int c, int n, int m)
+    {
+        return (r >= 0 && r < n && c >= 0 && c < m); 
+    }
+
+    int findMaxFish(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n, vector<int>(m,0));
+        int rd[] = {0,1,0,-1};
+        int cd[] = {1,0,-1,0};
+        int ans = 0;
+
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(grid[i][j] != 0 && vis[i][j] == 0)
+                {
+                    vis[i][j] = 1;
+                    queue<pair<int,int>> q;
+                    q.push({i,j});
+                    int temp = grid[i][j];
+
+                    while(!q.empty())
+                    {
+                        auto it = q.front();
+                        int r = it.first;
+                        int c = it.second;
+                        cout << r << " " << c << endl;
+                        q.pop();
+
+                        for(int k=0; k<4; k++)
+                        {
+                            int nr = r + rd[k];
+                            int nc = c + cd[k];
+
+                            if(check(nr,nc,n,m) && vis[nr][nc] == 0 && grid[nr][nc] != 0)
+                            {
+                                vis[nr][nc] = 1;
+                                temp += grid[nr][nc];
+                                q.push({nr,nc});
+                            }
+                        }
+                    }
+                    cout << "Temp - " << temp << endl;
+
+                    ans = max(temp, ans);
+                }
+            }
+        }
+        return ans;
+    }
+};
