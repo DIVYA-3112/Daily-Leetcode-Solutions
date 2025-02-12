b50ce82a54f492ad2d4a209e0159041c73300c00
DIVYA-3112 <113194039+DIVYA-3112@users.noreply.github.com>
2025-02-12 11:34:59 +0530

    Time: 51 ms (25.94%) | Memory: 75.4 MB (20.24%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -26,22 +26,9 @@ public:
         {
             if(it.second.size() >= 2)
             {
-                int max1 = 0, max2 = 0;
-                for(int i : it.second)
-                {
-                    cout << i << " ";
-                    if(i >= max1 && i >= max2)
-                    {
-                        max2 = max1;
-                        max1 = i;
-                    }
-                    else if(i >= max2)
-                    {
-                        max2 = i;
-                    }
-                }
-                cout << endl;
-                ans = max(max1+max2, ans);
+                sort(it.second.begin(), it.second.end());
+                int m = it.second.size();
+                ans = max(ans, it.second[m-1] + it.second[m-2]);
             }
         }
         return ans;
