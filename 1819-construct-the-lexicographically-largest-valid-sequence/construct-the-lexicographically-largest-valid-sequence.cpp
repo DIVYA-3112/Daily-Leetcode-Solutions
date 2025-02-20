24270f4ebf4a129df87a4ec193e18551040632e4
DIVYA-3112 <113194039+DIVYA-3112@users.noreply.github.com>
2025-02-20 19:38:30 +0530

    Time: 27 ms (3.65%) | Memory: 8.5 MB (57.98%) - LeetSync

---
+++ 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+
+    bool fun(int n, int len, vector<int> &vis, vector<int> &ans, int ind)
+    {
+        if(ind == len)
+        {
+            return true;
+        }
+        if(ans[ind] != 0)
+        {
+            if(fun(n,len,vis,ans,ind+1)) return true;
+        }
+        for(int i=n; i>=1; i--)
+        {
+            if(vis[i] == 0 && i != 1)
+            {
+                if(ans[ind] == 0 && (ind+i) < len && ans[ind+i] == 0)
+                {
+                    cout << ind << " - " << i << endl;
+                    vis[i] = 1;
+                    ans[ind] = i;
+                    ans[ind+i] = i;
+                    if(fun(n, len, vis, ans, ind+1)) return true;
+                    ans[ind+i] = 0;
+                    ans[ind] = 0;
+                    vis[i] = 0;
+                }
+            }
+            else if(vis[i] == 0 && i == 1 && ans[ind] == 0)
+            {
+                cout << ind << " - " << i << endl;
+                vis[i] = 1;
+                ans[ind] = 1;
+                if(fun(n, len, vis, ans, ind+1)) return true;
+                ans[ind] = 0;
+                vis[i] = 0;
+            }
+        }
+        return false;
+    }
+    vector<int> constructDistancedSequence(int n) {
+        int len = (n-1)*2 + 1;
+        vector<int> vis(n+1,0);
+        vector<int> ans(len,0);
+        fun(n, len, vis, ans, 0);
+        return ans;
+    }
+};
