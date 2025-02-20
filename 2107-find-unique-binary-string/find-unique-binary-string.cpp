4bde3859f3de3e2a08cf1257ff4f0204c6055add
DIVYA-3112 <113194039+DIVYA-3112@users.noreply.github.com>
2025-02-20 12:10:42 +0530

    Time: 7 ms (17.06%) | Memory: 12.7 MB (60.71%) - LeetSync

---
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+
+    int fun(string s, int n)
+    {
+        int ans = 0;
+        int k = 1;
+        for(int i=n-1; i>=0; i--)
+        {
+            ans += (k * (int)(s[i] - '0'));
+            k = (k << 1);
+        }
+        return ans;
+    }
+
+    string fun2(int i, int n)
+    {
+        string s = "";
+        while(i)
+        {
+            s += (char)((i % 2) + '0');
+            i /= 2;
+        }
+        while(s.size() < n)
+        {
+            s += "0";
+        }
+        reverse(s.begin(), s.end());
+        return s;
+    }
+    string findDifferentBinaryString(vector<string>& nums) {
+        int n = nums.size();
+        unordered_map<int,int> mp;
+        for(int i=0; i<n; i++)
+        {
+            int k = fun(nums[i], n);
+            mp[k] = 1;
+        }
+
+        for(auto it : mp)
+        {
+            cout << it.first << " - " << it.second << endl;
+        }
+
+        for(int i=0; i<n+1; i++)
+        {
+            if(mp.find(i) == mp.end())
+            {
+                return fun2(i,n);
+            }
+        }
+
+        return "0";
+    }
+};
