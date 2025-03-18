1ff55efbe21a65a4e960eb9eb582c397283e269c
DIVYA-3112 <113194039+DIVYA-3112@users.noreply.github.com>
2025-03-19 00:42:03 +0530

    Time: 184 ms (49.17%) | Memory: 148.9 MB (48.71%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
-    int fun(vector<int> &arr, int ind, int buy, int cnt, vector<vector<vector<int>>> &dp)
+    int fun(vector<int> &arr, int ind, int cnt, vector<vector<int>> &dp)
     {
         // base case
         int n = arr.size();
-        if(cnt == 0 && buy)
+        if(cnt == 0)
         {
             return 0;
         }
         if(ind == n-1)
         {
-            if(buy==0) return arr[ind];
-            else return 0;
+            return (cnt % 2 == 0) ? 0 : arr[ind]; 
         }
 
         // dp check
-        if(dp[ind][buy][cnt] != -1) return dp[ind][buy][cnt];
+        if(dp[ind][cnt] != -1) return dp[ind][cnt];
 
         // conditions;
         int ans;
-        if(buy)
+        if(cnt%2 == 0)
         {
-            int buyd = fun(arr, ind+1, 0, cnt, dp) - arr[ind];
-            int buynd = fun(arr, ind+1, 1, cnt, dp);
+            int buyd = fun(arr, ind+1, cnt-1, dp) - arr[ind];
+            int buynd = fun(arr, ind+1, cnt, dp);
             ans = max(buyd, buynd);
         }
         else
         {
-            int selld = fun(arr, ind+1, 1, cnt-1, dp) + arr[ind];
-            int sellnd = fun(arr, ind+1, 0, cnt, dp);
+            int selld = fun(arr, ind+1, cnt-1, dp) + arr[ind];
+            int sellnd = fun(arr, ind+1, cnt, dp);
             ans = max(selld, sellnd);
         }
-        return dp[ind][buy][cnt] = ans;
+        return dp[ind][cnt] = ans;
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>> (2, vector<int> (3, -1)));
-        return fun(prices, 0, 1, 2, dp);
+        vector<vector<int>> dp(n, vector<int> (5, -1));
+        return fun(prices, 0, 4, dp);
     }
 };
