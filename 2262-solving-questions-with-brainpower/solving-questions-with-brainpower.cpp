4527b92dff97e00a3edcf0df8d06199c2cc4533b
DIVYA-3112 <113194039+DIVYA-3112@users.noreply.github.com>
2025-04-01 10:48:19 +0530

    Time: 12 ms (28.33%) | Memory: 119.6 MB (94.99%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -18,7 +18,16 @@ public:
     }
     long long mostPoints(vector<vector<int>>& que) {
         int n = que.size();
-        vector<long long> dp(n, -1);
-        return fun(que, 0, dp);
+        vector<long long> dp(n+1,0);
+        dp[n] = 0;
+
+        for(int i=n-1; i>=0; i--)
+        {
+            int k = min(n, i+1+que[i][1]);
+            long long pick = dp[k] + que[i][0];
+            long long np = dp[i+1];
+            dp[i] = max(pick, np);
+        }
+        return dp[0];
     }
 };
