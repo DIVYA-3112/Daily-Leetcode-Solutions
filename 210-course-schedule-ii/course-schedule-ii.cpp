4b5e72dda5f0e184c58aff98be2986b62986c92f
DIVYA-3112 <113194039+DIVYA-3112@users.noreply.github.com>
2025-04-04 23:51:43 +0530

    Time: 3 ms (75.42%) | Memory: 18.6 MB (49.66%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+
+    bool toposort(vector<vector<int>> &adj, vector<int> &vis, vector<int> &temp, int ind, stack<int> &st)
+    {
+        vis[ind] = 1;
+        temp[ind] = 1;
+        for(int i : adj[ind])
+        {
+            if(temp[i]) return true;
+            if(!vis[i])
+            {
+                if(toposort(adj, vis, temp, i, st)) return true;
+            }
+        }
+        temp[ind] = 0;
+        st.push(ind);
+        return false;
+    }
+    vector<int> findOrder(int V, vector<vector<int>>& pre) {
+        // topo sort using dfs
+        // create adj list
+        vector<vector<int>> adj(V);
+        int n = pre.size();
+
+        for(int i=0; i<n; i++)
+        {
+            int u = pre[i][0];
+            int v = pre[i][1];
+
+            adj[v].push_back(u);
+        }
+
+        vector<int> vis(V,0);
+        vector<int> temp(V,0);
+        stack<int> st;
+        for(int i=0; i<V; i++)
+        {
+            if(!vis[i])
+            {
+                if(toposort(adj, vis, temp, i, st)) return {};
+            }
+        }
+
+        vector<int> ans;
+        while(!st.empty())
+        {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        return ans;
+    }
+};
